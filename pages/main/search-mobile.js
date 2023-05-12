29a2022ddc0057a36ab107d818b3e14cddb1f2c3
Chferchko <3abolotskih@gmail.com>
2023-05-12 15:18:38 +0600

    refactor: hovers & search-mobile

--- pages/main/search-mobile.js
+++ pages/main/search-mobile.js
@@ -1,12 +1,13 @@
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const cancel = document.getElementById("search-cancel");
 
 function toggleActiveClass() {
 	form.classList.toggle("active");
 };
 
 function toggleDispaySearch(e) {
-	if (e.target == form || form.classList.contains("active") && !search.contains(e.target)) toggleActiveClass();
+	if (e.target == form || e.target == cancel) toggleActiveClass();
 };
 
 function clickHandler(e) {
