3efac957fb94f9f41e693799d9fddf56a4735048
Chferchko <3abolotskih@gmail.com>
2023-05-15 19:38:18 +0600

    feat: add catalog popup

---
+++ pages/main/catalog-button.js
@@ -0,0 +1,23 @@
+const catalog = document.getElementById("catalog");
+const catalog_button = document.getElementById("catalog-button");
+
+function toggleVisibleClass() {
+	catalog.classList.toggle("visible");
+};
+
+function toggleStatusButtonClass() {
+	catalog_button.classList.toggle("cancel");
+};
+
+function toggleDisplayCatalog(e) {
+	if (e.target == catalog_button) {
+		toggleVisibleClass();
+		toggleStatusButtonClass();
+	}
+};
+
+function clickHandler(e) {
+	toggleDisplayCatalog(e);
+};
+
+document.addEventListener("click", clickHandler);
